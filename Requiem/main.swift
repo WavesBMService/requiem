73088f299a4a969b869285faf47d0db952a6583b
Martin Kim Dung-Pham <kim@elbedev.com>
2018-02-07 21:05:04 +0100

    Add command line app 'requiem'

---
+++ Requiem/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Requiem
+//
+//  Created by Martin Kim Dung-Pham on 07.02.18.
+//  Copyright © 2018 elbedev.com. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
